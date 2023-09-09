a767d2e6255cdd213c4095e1d06cb0c68ce14b6d
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-09 12:06:35 +0200

    modal terminado

--- components/CustomModal.js
+++ components/CustomModal.js
@@ -12,65 +12,76 @@ const CustomModal = ({
 }) => {
     return (
       <Modal animationType='slide' visible={modalVisible}>
-        <View className={styles.modalContainer}>
-          <View className={styles.modalContent}>
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              {/* TITLE */}
+              <View style={styles.titleContainer}>
+                <Text style={styles.modalTitle}>{title}</Text>
+              </View>
 
-            {/* TITLE */}
-            <View className={styles.modalTitle}>
-              <Text>{title}</Text>
+              {/* MESSAGE */}
+              <View style={styles.messageContainer}>
+                <Text style={styles.modalMessage}>{message}</Text>
+              </View>
+
+              {/* BUTTONS CONTAINER */}
+              <View style={styles.buttonsContainer}>
+                <TouchableOpacity
+                  onPress={() => {
+                    acceptButtonFunction()
+                    setShowModal(false)
+                  }}  
+                  style={styles.appButtonContainer}
+                >
+                  <Text style={styles.appButtonText}>{acceptButtonName}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                  onPress={() => setShowModal(false)}  
+                  style={styles.appButtonContainer}
+                >
+                  <Text style={styles.appButtonText}>{cancelButtonName}</Text>
+                </TouchableOpacity>
+              </View>
+
             </View>
-
-            {/* MESSAGE */}
-            <View className={styles.modalMessage}>
-              <Text>{message}</Text>
-            </View>
-
-            {/* BUTTONS CONTAINER */}
-            <View styles={styles.buttonsContainer}>
-              <TouchableOpacity
-                onPress={() => {
-                  acceptButtonFunction()
-                  setShowModal(false)
-                }}  
-                style={styles.appButtonContainer}
-              >
-                <Text style={styles.appButtonText}>{acceptButtonName}</Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity 
-                onPress={() => setShowModal(false)}  
-                style={styles.appButtonContainer}
-              >
-                <Text style={styles.appButtonText}>{cancelButtonName}</Text>
-              </TouchableOpacity>
-            </View>
-
-          </View>
         </View>
       </Modal>
+
     );
 }
 
 const styles = StyleSheet.create({
   modalContainer: {
+    height:'100%',
+    width: '100%',
+    backgroundColor:'#2f2f2f',
     flex:1,
+    flexDirection: 'column',
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
   },
   modalContent: {
-    backgroundColor:'#2f2f2f',
-    padding: 35,
+    borderRadius: 10,
+    height: '20%',
+    width: '60%',
+    backgroundColor:'white',
     justifyContent:'center',
     alignItems:'center'
   },
   modalTitle: {
-    fontSize: 0,
+    fontSize: 50,
+    marginBottom: 30
   },
   modalMessage: {
     fontSize: 30,
   },
   buttonsContainer: {
-    flexDirection:'column',
+    flexDirection:'row',
+    width: '90%',
+    justifyContent: 'space-around',
+    marginTop: 50,
+    height:'auto'
   },
   appButtonContainer: {
     elevation: 8,
